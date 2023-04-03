2a2abd215e996457160f647be3ae81be9931504f
Austin Wang <yyaustin@bu.edu>
2023-04-02 23:11:57 -0400

    Finish exercise 6.22

--- src/App.js
+++ src/App.js
@@ -1,10 +1,63 @@
 import axios from "axios"
-import { useQuery } from "react-query"
+import { QueryClient, useMutation, useQuery, useQueryClient } from "react-query"
+
+const AnecdoteForm = () => {
+  const queryClient = useQueryClient()
+  
+  const { mutate } = useMutation("anecdotes", (content) => {
+    return axios.post("http://localhost:3003/anecdotes", { content, votes: 0 }).then(res => res.data)
+  }, {
+    onMutate: (content) => {
+      if (content.length < 5) {
+        throw new Error("at least 5 characters long")
+      }
+      return { validationError: null}
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries("anecdotes")
+    }
+  })
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    const formData = new FormData(event.target)
+    const content = formData.get("content")
+    mutate(content)
+  }
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input name="content" />
+        <button type="submit">add</button>
+      </form>
+    </div>
+  )
+}
 
 const App = () => {
+  const queryClient = useQueryClient()
+  
   const { isLoading, error, data } = useQuery("anecdotes", () => {
     return axios.get("http://localhost:3003/anecdotes").then(res => res.data)
+  }, {
+    onSuccess: () => {
+      queryClient.invalidateQueries("anecdotes")
+    }
   })
+
+  const { mutate } = useMutation("anecdotes", (newAnecdote) => {
+    return axios.put(`http://localhost:3003/anecdotes/${newAnecdote.id}`, newAnecdote)
+  })
+
+  const handleVote = (anecdote) => {
+    const newAnecdote = {
+      ...anecdote,
+      votes: anecdote.votes + 1
+    }
+    mutate(newAnecdote)
+    console.log("haha")
+  }
 
   if (isLoading) {
     return (
@@ -20,8 +73,9 @@ const App = () => {
   
   return (
     <div>
+      <AnecdoteForm />
       {data.map((anecdote) => (
-        <div>
+        <div key={anecdote.id} onClick={() => handleVote(anecdote)}>
           {anecdote.content} -- has {anecdote.votes} votes 
         </div>
       ))}
